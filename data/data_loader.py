a93440dbed03e7d55dc3f42d1e433768d65c7d57
squilliam12 <wfan@wm.edu>
2026-03-13 21:40:06 -0700

    Initialize function to load 10 year treasury data

--- data/data_loader.py
+++ data/data_loader.py
@@ -1,5 +1,8 @@
 import yfinance as yf
 from datetime import date
+import os
+from dotenv import load_dotenv
+from fredapi import Fred
 
 def load_stock_data(ticker: str, start_date: str = '2000-01-01', end_date: str = date.today().strftime('%Y-%m-%d'), interval: str = '1d'):
     """
@@ -33,3 +36,16 @@ def load_sp500_data(start_date: str = '2000-01-01', end_date: str = date.today()
     """
     sp = load_stock_data('^GSPC', start_date, end_date, interval)
     return sp
+
+def load_10_year_treasury_data():
+    """"
+    Load historical 10-year Treasury yield data from FRED.
+
+    Returns:
+    - Series: A Series containing the historical 10-year Treasury yield data.
+    """
+    load_dotenv()
+    fred_api_key = os.getenv('FRED')
+    fred = Fred(api_key=fred_api_key)
+    treasury_10 = fred.get_series('DGS10')
+    return treasury_10
