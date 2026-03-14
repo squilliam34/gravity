7c46e79e5090a2c9acd7e3ebace217dae7c64ea6
squilliam12 <wfan@wm.edu>
2026-03-13 21:16:41 -0700

    Add S&P data loader

--- data/data_loader.py
+++ data/data_loader.py
@@ -18,3 +18,18 @@ def load_stock_data(ticker: str, start_date: str = '2000-01-01', end_date: str =
     stock_data = stock.history(start=start_date, end=end_date, interval=interval)
     stock_data.index = stock_data.index.date  
     return stock_data
+
+def load_sp500_data(start_date: str = '2000-01-01', end_date: str = date.today().strftime('%Y-%m-%d'), interval: str = '1d'):
+    """
+    Load historical S&P 500 index data.
+
+    Parameters:
+    - start_date (str): The start date for the historical data in 'YYYY-MM-DD' format.
+    - end_date (str): The end date for the historical data in 'YYYY-MM-DD' format.
+    - interval (str): The data interval (e.g., '1d' for daily, '1wk' for weekly).
+
+    Returns:
+    - DataFrame: A DataFrame containing the historical S&P 500 index data.
+    """
+    sp = load_stock_data('^GSPC', start_date, end_date, interval)
+    return sp
