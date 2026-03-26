ba40fab61c83acbb6dd892027dc8a847c7a05388
squilliam12 <wfan@wm.edu>
2026-03-25 23:54:24 -0400

    Implement function to create market cap matrix

--- src/mass/mass.py
+++ src/mass/mass.py
@@ -1,5 +1,6 @@
 import yfinance as yf
 import pandas as pd
+import numpy as np
 
 def calculate_monthly_market_cap(ticker: str, 
                                  start: str = '2000-01-01'
@@ -27,3 +28,27 @@ def calculate_monthly_market_cap(ticker: str,
     prices = company.history(start = start)['Close']
     market_caps = (shares*prices).resample('ME').mean()
     return market_caps
+
+def create_market_cap_matrix(tickers: list[str]) -> np.ndarray:
+    """
+    Creates a TxN matrix of monthly market caps, where T is the number
+    of time periods, and N is the number of stocks in the universe.
+    Market caps are scaled by a factor of 1e9 to avoid future calculation
+    overflow
+
+    Parameters:
+    - tickers (list[str]): A list of stock tickers to populate the matrix with
+
+    Returns:
+    - np.ndarray: A 2D matrix containing market caps for N stocks across T periods
+    of time
+    """
+    # Need to put monthly market caps in df first 
+    # due to differences in lengths
+    series_dict = {}
+    for ticker in tickers:
+        # Scales market caps by a factor of 1B to prevent overflow
+        s = calculate_monthly_market_cap(ticker) / 1e9
+        series_dict[ticker] = s
+        market_cap_matrix = pd.DataFrame(series_dict)
+    return market_cap_matrix.to_numpy()
