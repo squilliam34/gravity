a1e49ea577b6fa404c543cdbe4080754b62bb702
squilliam12 <wfan@wm.edu>
2026-06-13 01:04:58 -0400

    Implement lambda calculation

--- src/distance/distance.py
+++ src/distance/distance.py
@@ -1,4 +1,6 @@
 import numpy as np
+import pandas as pd
+from data.data_loader import load_prices, load_factor_data
 
 def sigmoid(vix: np.ndarray, k: int = 0.05, threshold: int=20) -> np.ndarray:
     """
@@ -14,3 +16,27 @@ def sigmoid(vix: np.ndarray, k: int = 0.05, threshold: int=20) -> np.ndarray:
     np.ndarray: An array containing the transformed VIX value at each point in time.
     """
     return 1/(1 + np.exp(-k*(vix-threshold)))
+
+def get_lambda(start_date: str = '2000-01-01', 
+                end_date: str = date.today().strftime('%Y-%m-%d'), 
+                interval: str = '1d') -> pd.DataFrame:
+    """
+    Calculates lambda for the given time period by applying a sigmoid
+    transformation to the VIX.
+
+    Args:
+    start_date (str): The start date for the data in 'YYYY-MM-DD' format.
+    end_date (str): The end date for the data in 'YYYY-MM-DD' format.
+    interval (str): The data interval (e.g., '1d' for daily, '1wk' for weekly).
+
+    Returns:
+    pd.DataFrame: A DataFrame containing the series of lambda values to use for 
+      weighting in final distance calculation.
+    """
+    vix = load_prices('^VIX', start_date=start_date, end_date=end_date, interval=interval)
+    vix.columns=['VIX']
+
+    # Apply sigmoid transformation
+    sig = sigmoid(vix['VIX'].to_numpy())
+    
+    return pd.DataFrame(sig, index=vix.index, columns=['lambda'])
