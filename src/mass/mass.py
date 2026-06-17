02a16b9ca9dc624b7fc98213f9ebaa4d88119df1
squilliam12 <wfan@wm.edu>
2026-06-17 09:38:56 -0400

    Add end date to daily market cap

--- src/mass/mass.py
+++ src/mass/mass.py
@@ -4,8 +4,9 @@ import numpy as np
 from datetime import date
 
 def calculate_daily_market_cap(ticker: str, 
-                                 start: str = '2000-01-01'
-                                 ) -> pd.Series:
+                               start_date: str = '2000-01-01',
+                               end_date: str = date.today().strftime('%Y-%m-%d')
+                              ) -> pd.Series:
     """
     Calculate a series of daily market caps for a given stock using its ticker.
 
@@ -16,18 +17,21 @@ def calculate_daily_market_cap(ticker: str,
 
     Args:
     ticker (str): The stock symbol to retrieve the historic market caps for.
-    start (str): The date to start calculating the market capitalization from in the format 
-      YYYY-MM-DD.
-
+    start_date (str): The start date for the data in 'YYYY-MM-DD' format.
+    end_date (str): The end date for the data in 'YYYY-MM-DD' format.
+    
     Returns:
-    pd.Series: A daily series containing a company's market capitalization at different points 
-      in time.
+    pd.Series: A daily series containing a company's market capitalization at different 
+    points in time.
     """
     company = yf.Ticker(ticker)
     income_stmt = company.quarterly_income_stmt
     shares = income_stmt[income_stmt.columns[0]]['Basic Average Shares']
-    prices = company.history(start = start)['Close']
-    market_caps = (shares*prices)
+    prices = company.history(start = start_date, end = end_date)['Close']
+    
+    # Log market caps to compress space
+    market_caps = np.log((shares*prices))
+    market_caps = market_caps.rename(ticker)
     return market_caps
 
 def create_market_cap_matrix(tickers: list[str]) -> np.ndarray:
