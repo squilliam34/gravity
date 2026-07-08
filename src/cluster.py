fabe6bb728f1c22de0ca9897dcbf9d6d9cf9909d
squilliam12 <wfan@wm.edu>
2026-07-08 19:03:56 -0400

    __str__

--- src/cluster.py
+++ src/cluster.py
@@ -2,7 +2,7 @@
 
 class Cluster:
     
-    def __init__(self, label, tickers, date, embeddings=None, market_caps=None, factor_data=None):
+    def __init__(self, label:int, tickers:list[str], date:str, embeddings=None, market_caps=None, factor_data=None):
 
         self.label = label
         self.tickers = tickers
@@ -11,3 +11,6 @@ class Cluster:
         self.embeddings = embeddings
         self.market_caps = market_caps
         self.factor_data = factor_data
+
+    def __str__(self):
+        return f'Cluster {self.label}: {', '.join(self.tickers)}'
