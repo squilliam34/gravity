ff5d6afc5d9f5ae474b51d01408a751f92dc9017
squilliam12 <wfan@wm.edu>
2026-07-10 17:51:32 -0400

    New data loop

--- script/data_pipeline_v2.py
+++ script/data_pipeline_v2.py
@@ -27,6 +27,9 @@ logging.getLogger('yfinance').setLevel(logging.CRITICAL)
 ### Change dimensionality reduction model ###
 MODEL = 'spectral'
 
+### Recompute existing gravity? ###
+FORCE_RECOMPUTE = False
+
 ########################################
 # PART 1: Import tickers
 ########################################
@@ -146,53 +149,15 @@ for start_date, end_date in tqdm(
     desc='Processing intervals',
     unit='interval'
 ):
-
-    factor_path = Path(f'./data/S&P500/factor_distances/factor_distance_{start_date}_{end_date}.csv')
-    market_cap_path = Path(f'./data/S&P500/market_caps/market_caps_{start_date}_{end_date}.csv')
-
     tqdm.write(f'Processing {start_date} → {end_date}')
 
-    ####################################
-    # Factor distances
-    ####################################
-
-    if factor_path.exists():
-        tqdm.write(f'Factor distances already exist: {start_date} → {end_date}')
-
-    else:
-        tqdm.write('Loading factor data...')
-        factor_data = load_factor_data(
-            tickers=tickers,
+    for cluster in tqdm(
+        clusters,
+        desc='Processing clusters',
+        unit='cluster'
+    ):
+        cluster.get_gravity(
             start_date=start_date,
-            end_date=end_date
+            end_date=end_date,
+            force_recompute=FORCE_RECOMPUTE
         )
-        betas = calculate_rolling_betas(
-            data=factor_data,
-            tickers=tickers
-        )
-        factor_distance = compute_distances(
-            betas=betas
-        )
-        factor_distance.to_csv(
-            f'./data/S&P500/factor_distances/factor_distance_{start_date}_{end_date}.csv'
-        )
-        tqdm.write('Factor distances saved.')
-
-    ####################################
-    # Market cap products
-    ####################################
-
-    if market_cap_path.exists():
-        tqdm.write(f'Market caps already exist: {start_date} → {end_date}')
-
-    else:
-        tqdm.write('Calculating market caps...')
-        market_caps = create_market_cap_df(
-            tickers=tickers,
-            start_date=start_date,
-            end_date=end_date
-        )
-        market_caps.to_csv(
-            f'./data/S&P500/market_caps/market_caps_{start_date}_{end_date}.csv'
-        )
-        tqdm.write('Market caps saved.')
