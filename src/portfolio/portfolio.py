3b0d01d2cd7b5fffd7327e17391b701403a32094
squilliam12 <wfan@wm.edu>
2026-07-17 20:24:21 -0400

    Implement cache

--- src/portfolio/portfolio.py
+++ src/portfolio/portfolio.py
@@ -3,6 +3,8 @@ from dataclasses import dataclass
 import pandas as pd
 from typing import Dict, Optional
 import yfinance as yf
+from pathlib import Path
+from config import DATA_DIR
 
 @dataclass(frozen=true)
 class HoldingPeriod:
@@ -18,30 +20,64 @@ class HoldingPeriod:
     portfolio_returns: Optional[pd.Series] = field(default=None, init=False, repr=False)
     def load_prices(self, benchmark='^GSPC'):
         start, end = self.period
+        period_str = f'{pd.Timestamp(start).date()}_{pd.Timestamp(end).date()}'
 
-        tickers = list(self.holdings.keys())
+        cache_dir = (
+            DATA_DIR
+            / 'portfolio'
+            / period_str
+            / 'prices'
+        )
 
-        self.prices = yf.download(
-            tickers,
-            start=start,
-            end=end,
-            auto_adjust=True,
-            progress=False
-        )['Close']
+        cache_dir.mkdir(parents=True, exist_ok=True)
 
-        self.benchmark_prices = yf.download(
-            benchmark,
-            start=start,
-            end=end,
-            auto_adjust=True,
-            progress=False
-        )['Close']
+        price_path = cache_dir / 'prices.csv'
+        benchmark_path = cache_dir / 'benchmark.csv'
 
+        # Check cache
+        if price_path.exists(): 
+            self.prices = pd.read_csv(
+                price_path, 
+                index_col=0, 
+                parse_dates=True
+            )
+
+        else:
+            tickers = list(self.holdings.keys())
+
+            self.prices = yf.download(
+                tickers,
+                start=start,
+                end=end,
+                auto_adjust=True,
+                progress=False
+            )['Close']
+
+            self.prices.to_csv(price_path)
+
+        if benchmark_path.exists():
+
+            self.benchmark_prices = pd.read_csv(
+                benchmark_path,
+                index_col=0,
+                parse_dates=True
+            ).squeeze('columns')
+
+        else:
+            self.benchmark_prices = yf.download(
+                benchmark,
+                start=start,
+                end=end,
+                auto_adjust=True,
+                progress=False
+            )['Close']
+
+            self.benchmark_prices.to_csv(benchmark_path)
         return self.prices
 
     def calculate_returns(self):
         if self.prices is None:
-            raise ValueError("Prices have not been loaded.")
+            raise ValueError('Prices have not been loaded.')
 
         self.returns = self.prices.pct_change().dropna()
 
@@ -60,8 +96,6 @@ class HoldingPeriod:
         )
 
         return self.portfolio_returns
-
-    
 
 class Portfolio:
     def __init__(
